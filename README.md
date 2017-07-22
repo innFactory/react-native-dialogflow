f775327787e84478ca1cd4d3497d12a79549b830
spoeck <toni.spoeck@web.de>
2017-07-22 20:41:13 +0200

    feat: support for android

--- README.md
+++ README.md
@@ -1,2 +1,118 @@
 # react-native-api-ai
 A React-Native Bridge for the Google API AI SDK
+
+Currently we are supporting android only. The support for ios will be released the next days.
+
+
+## Install
+
+Add react-native-api-ai and link it:
+```
+npm install --save react-native-api-ai
+
+react-native link react-native-api-ai
+```
+
+
+## Usage
+Import ApiAi:
+```javascript
+import ApiAi from "react-native-api-ai";
+```
+
+Set the `clientAccessToken` and the language in your contructor:
+```javascript
+ constructor(props) {
+        super(props);
+
+        ApiAi.setConfiguration(
+          "4xxxxxxxe90xxxxxxxxc372", ApiAi.LANG_GERMAN
+        );
+    }
+
+```
+
+Start listening with integrated speech recognition:
+```javascript
+   <Button onPress={() => {
+            ApiAi.startListening(result=>{
+                console.log(result);
+            }, error=>{
+                console.log(error);
+            });
+        }}
+   />
+```
+
+Usage of `onListeningStarted`, `onListeningCanceled`, `onListeningFinished` and `onAudioLevel`:
+```javascript
+   <Button onPress={() => {
+
+            ApiAi.onListeningStarted(()=>{
+                console.log("listening started");
+            });
+
+            ApiAi.onListeningCanceled(()=>{
+                console.log("listening canceled");
+            });
+
+            ApiAi.onListeningFinished(()=>{
+                console.log("listening finished");
+            });
+
+            ApiAi.onAudioLevel(level=>{
+                console.log(level);
+            });
+
+
+            ApiAi.startListening(result=>{
+                console.log(result);
+            }, error=>{
+                console.log(error);
+            });
+        }}
+   />
+```
+Note: Make sure you are setting the callbacks before startListening again every single time. Don't set in e.g. constructor or componentsDidMount if you are executing startListening more than one times.
+
+Using your own speech recognition:
+```javascript
+   <Button onPress={() => {
+           ApiAi.requestQuery("Some text for your api-ai agent", result=>console.log(result), error=>console.log(error));
+        }}
+   />
+```
+
+## Supported Languages
+Set the language in your configuration:
+```javascript
+ApiAi.setConfiguration("4xxxxxxxe90xxxxxxxxc372", ApiAi.LANG_GERMAN);
+```
+* LANG_CHINESE_CHINA
+* LANG_CHINESE_HONGKONG
+* LANG_CHINESE_TAIWAN
+* LANG_DUTCH
+* LANG_ENGLISH
+* LANG_ENGLISH_GB
+* LANG_ENGLISH_US
+* LANG_FRENCH
+* LANG_GERMAN
+* LANG_ITALIAN
+* LANG_JAPANESE
+* LANG_KOREAN
+* LANG_PORTUGUESE
+* LANG_PORTUGUESE_BRAZIL
+* LANG_RUSSIAN
+* LANG_SPANISH
+* LANG_UKRAINIAN
+
+## Methods
+| name                  | param1    | param2    | param3    |
+| --------------------- | --------- | --------- | --------- |
+| `setConfiguration`    | clientAccessToken: String | languageTag: String | |
+| `startListening`      | resultCallback: (result: object)=>{} | errorCallback: (error: object)=>{}  | |
+| `requestQuery`           | query: String |  resultCallback: (result: object)=>{} | errorCallback: (error: object)=>{}   |
+| `onListeningStarted`            | callback: ()=>{}    | | |
+| `onListeningCanceled`            | callback: ()=>{}    || |
+| `onListeningFinished`            | callback: ()=>{}    | | |
+| `onAudioLevel`            | callback: (level: number)=>{}    || |
--- android/src/main/java/de/innfactory/apiai/RNApiAiModule.java
+++ android/src/main/java/de/innfactory/apiai/RNApiAiModule.java
@@ -1,38 +1,69 @@
 package de.innfactory.apiai;
 
-import com.facebook.react.bridge.NativeModule;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.google.gson.Gson;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import ai.api.AIListener;
+import ai.api.AIServiceException;
 import ai.api.android.AIConfiguration;
+import ai.api.android.AIDataService;
 import ai.api.android.AIService;
 import ai.api.model.AIError;
+import ai.api.model.AIRequest;
 import ai.api.model.AIResponse;
 import ai.api.model.Result;
-import com.google.gson.JsonElement;
-
-import android.widget.Toast;
-
-import java.util.Map;
 
 /**
  * Created by Anton Spöck on 2017-07-21
  **/
 public class RNApiAiModule extends ReactContextBaseJavaModule implements AIListener {
 
+    private static final String TAG = "ApiAi";
+
+    private static final String LANG_CHINESE_CHINA = "LANG_CHINESE_CHINA";
+    private static final String LANG_CHINESE_HONGKONG = "LANG_CHINESE_HONGKONG";
+    private static final String LANG_CHINESE_TAIWAN = "LANG_CHINESE_TAIWAN";
+    private static final String LANG_DUTCH = "LANG_DUTCH";
+    private static final String LANG_ENGLISH = "LANG_ENGLISH";
+    private static final String LANG_ENGLISH_GB = "LANG_ENGLISH_GB";
+    private static final String LANG_ENGLISH_US = "LANG_ENGLISH_US";
+    private static final String LANG_FRENCH = "LANG_FRENCH";
+    private static final String LANG_GERMAN = "LANG_GERMAN";
+    private static final String LANG_ITALIAN = "LANG_ITALIAN";
+    private static final String LANG_JAPANESE = "LANG_JAPANESE";
+    private static final String LANG_KOREAN = "LANG_KOREAN";
+    private static final String LANG_PORTUGUESE = "LANG_PORTUGUESE";
+    private static final String LANG_PORTUGUESE_BRAZIL = "LANG_PORTUGUESE_BRAZIL";
+    private static final String LANG_RUSSIAN = "LANG_RUSSIAN";
+    private static final String LANG_SPANISH = "LANG_SPANISH";
+    private static final String LANG_UKRAINIAN = "LANG_UKRAINIAN";
+
 
     private AIService aiService;
-    final AIConfiguration config = new AIConfiguration("57b6ce865e6e4b138a74a88cfd8bc526",
-            AIConfiguration.SupportedLanguages.German,
+    private AIConfiguration config = new AIConfiguration("",
+            AIConfiguration.SupportedLanguages.DEFAULT,
             AIConfiguration.RecognitionEngine.System);
+    private AIDataService aiDataService;
+
+    private Callback onResultCallback;
+    private Callback onErrorCallback;
+    private Callback onListeningStartedCallback;
+    private Callback onListeningCanceledCallback;
+    private Callback onListeningFinishedCallback;
+    private Callback onAudioLevelCallback;
+
 
     public RNApiAiModule(ReactApplicationContext reactContext) {
         super(reactContext);
-
-
     }
 
 
@@ -41,67 +72,218 @@ public class RNApiAiModule extends ReactContextBaseJavaModule implements AIListe
         return "ApiAi";
     }
 
+    @ReactMethod
+    public void setConfiguration(String clientAccessToken, String languageTag) {
+        config = new AIConfiguration(clientAccessToken, AIConfiguration.SupportedLanguages.fromLanguageTag(languageTag), AIConfiguration.RecognitionEngine.System);
+    }
+
 
     @ReactMethod
-    public void startListening() {
+    public void startListening(Callback onResult, Callback onError){//, Callback onListeningStarted, Callback onListeningCanceled,  Callback onListeningFinished,  Callback onAudioLevel) {
+
+        onResultCallback = onResult;
+        onErrorCallback = onError;
+
         getCurrentActivity().runOnUiThread(new Runnable() {
 
             public void run() {
 
-                Toast.makeText(getReactApplicationContext(), "startListening", Toast.LENGTH_SHORT).show();
                 aiService = AIService.getService(getReactApplicationContext(), config);
                 aiService.setListener(RNApiAiModule.this);
                 aiService.startListening();
-
             }
         });
     }
 
-    @Override
-    public void onResult(AIResponse response) {
-        final Result result = response.getResult();
-
-        // Get parameters
-        String parameterString = "";
-        if (result.getParameters() != null && !result.getParameters().isEmpty()) {
-            for (final Map.Entry<String, JsonElement> entry : result.getParameters().entrySet()) {
-                parameterString += "(" + entry.getKey() + ", " + entry.getValue() + ") ";
-            }
-        }
-
-        final String parameterStringFinal = parameterString;
-
+    @ReactMethod
+    public void stopListening() {
         getCurrentActivity().runOnUiThread(new Runnable() {
 
             public void run() {
-                Toast.makeText(getReactApplicationContext(), "Query:" + result.getResolvedQuery() +
-                        "\nAction: " + result.getAction() +
-                        "\nParameters: " + parameterStringFinal, Toast.LENGTH_LONG).show();
+
+               if (aiService != null) {
+                   aiService.stopListening();
+               }
             }
         });
+    }
 
+
+    @ReactMethod
+    public void cancel() {
+        getCurrentActivity().runOnUiThread(new Runnable() {
+
+            public void run() {
+
+                if (aiService != null) {
+                    aiService.cancel();
+                }
+            }
+        });
+    }
+
+
+
+    @Override
+    public void onResult(AIResponse response) {
+
+        final Result result = response.getResult();
+
+        if (onResultCallback != null) {
+            Gson gson = new Gson();
+            try {
+                onResultCallback.invoke(gson.toJson(result));
+            } catch (Exception e) {
+                Log.e(TAG, e.getMessage(), e);
+            }
+        }
 
     }
 
     @Override
-    public void onError(AIError error) {
-        Toast.makeText(getReactApplicationContext(), error.toString(), Toast.LENGTH_LONG).show();
+    public void onError(final AIError error) {
+
+        if (onErrorCallback != null) {
+            Gson gson = new Gson();
+
+            try {
+                onErrorCallback.invoke(gson.toJson(error));
+            } catch (Exception e) {
+                Log.e(TAG, e.getMessage(), e);
+            }
+        }
     }
 
-    @Override
-    public void onAudioLevel(float level) {
-
+    @ReactMethod
+    public void onListeningStarted(Callback callback) {
+        onListeningStartedCallback = callback;
     }
 
     @Override
     public void onListeningStarted() {
+        if (onListeningStartedCallback != null) {
+            try {
+                onListeningStartedCallback.invoke();
+            } catch (Exception e) {
+                Log.e(TAG, e.getMessage(), e);
+            }
+        }
+    }
+
+    @ReactMethod
+    public void onListeningCanceled(Callback callback) {
+        onListeningCanceledCallback = callback;
     }
 
     @Override
     public void onListeningCanceled() {
+        if (onListeningCanceledCallback != null) {
+            try {
+                onListeningCanceledCallback.invoke();
+            } catch (Exception e) {
+                Log.e(TAG, e.getMessage(), e);
+            }
+        }
+    }
+
+    @ReactMethod
+    public void onListeningFinished(Callback callback) {
+        onListeningFinishedCallback = callback;
     }
 
     @Override
     public void onListeningFinished() {
+        if (onListeningFinishedCallback != null) {
+            try {
+                onListeningFinishedCallback.invoke();
+            } catch (Exception e) {
+                Log.e(TAG, e.getMessage(), e);
+            }
+        }
     }
+
+
+    @ReactMethod
+    public void onAudioLevel(Callback callback) {
+        onAudioLevelCallback = callback;
+    }
+
+    @Override
+    public void onAudioLevel(float level) {
+        if (onAudioLevelCallback != null) {
+            try {
+                onAudioLevelCallback.invoke(level);
+            } catch (Exception e) {
+                Log.e(TAG, e.getMessage(), e);
+            }
+        }
+
+    }
+
+
+    @ReactMethod
+    public void requestQuery(String query, Callback onResult, Callback onError) {
+
+        onResultCallback = onResult;
+        onErrorCallback = onError;
+
+        if (aiDataService == null) {
+            aiDataService = new AIDataService(getReactApplicationContext(), config);
+        }
+
+        final AIRequest aiRequest = new AIRequest();
+        aiRequest.setQuery(query);
+
+
+
+        new AsyncTask<AIRequest, Void, AIResponse>() {
+            @Override
+            protected AIResponse doInBackground(AIRequest... requests) {
+                final AIRequest request = requests[0];
+                try {
+                    final AIResponse response = aiDataService.request(aiRequest);
+                    return response;
+                } catch (AIServiceException e) {
+                    Gson gson= new Gson();
+                    try {
+                        onErrorCallback.invoke(gson.toJson(e));
+                    } catch (Exception e1) {
+                        Log.e(TAG, e.getMessage(), e);
+                    }
+                }
+                return null;
+            }
+            @Override
+            protected void onPostExecute(AIResponse aiResponse) {
+                if (aiResponse != null) {
+                    onResult(aiResponse);
+                }
+            }
+        }.execute(aiRequest);
+    }
+
+    @Override
+    public Map<String, Object> getConstants() {
+        final Map<String, Object> constants = new HashMap<>();
+        constants.put(LANG_CHINESE_CHINA, "zh-CN");
+        constants.put(LANG_CHINESE_HONGKONG, "zh-HK");
+        constants.put(LANG_CHINESE_TAIWAN, "zh-TW");
+        constants.put(LANG_DUTCH, "nl");
+        constants.put(LANG_ENGLISH, "en");
+        constants.put(LANG_ENGLISH_GB, "en-GB");
+        constants.put(LANG_ENGLISH_US, "en-US");
+        constants.put(LANG_FRENCH, "fr");
+        constants.put(LANG_GERMAN, "de");
+        constants.put(LANG_ITALIAN, "it");
+        constants.put(LANG_JAPANESE, "ja");
+        constants.put(LANG_KOREAN, "ko");
+        constants.put(LANG_PORTUGUESE, "pt");
+        constants.put(LANG_PORTUGUESE_BRAZIL, "pt-BR");
+        constants.put(LANG_RUSSIAN, "ru");
+        constants.put(LANG_SPANISH, "es");
+        constants.put(LANG_UKRAINIAN, "uk");
+        return constants;
+    }
+
+
 }
